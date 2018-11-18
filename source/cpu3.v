e2beeee6f3f99fd3165e867775fd3b509b3a7009
hitohira <hitohirasakuno@gmail.com>
2018-11-18 12:30:20 +0900

    分岐予測はなし

--- source/cpu3.v
+++ source/cpu3.v
@@ -21,21 +21,24 @@ module cpu3(
 	input wire io_out_rdy,
 	output wire io_out_vld,
 
-	input wire [4:0] io_err,
+	input wire [4:0] io_err
 	);
 	
 	localparam st_begin =  3'b001;
 	localparam st_normal = 3'b010;
 
-	localparam mod_b =   9'b000000001;
-	localparam mod_io =  9'b000000010;
-	localparam mod_mem = 9'b000000100;
-	localparam mod_alu = 9'b000001000;
-	localparam mod_mv =  9'b000010000;
-	localparam mod_fab = 9'b000100000;
-	localparam mod_fml = 9'b001000000;
-	localparam mod_fds = 9'b010000000;
-	localparam mod_fet = 9'b100000000;
+	localparam mod_b =    10'b0000000001;
+	localparam mod_io =   10'b0000000010;
+	localparam mod_mem =  10'b0000000100;
+	localparam mod_alu =  10'b0000001000;
+	localparam mod_alu2 = 10'b0000010000;
+	localparam mod_mv =   10'b0000100000;
+	localparam mod_fab =  10'b0001000000;
+	localparam mod_fml =  10'b0010000000;
+	localparam mod_fds =  10'b0100000000;
+	localparam mod_fet =  10'b1000000000;
+
+	localparam mask = ~({31'b0,1'b1,31'b0,1'b1});
 
 	reg [13:0] pc;
 	reg [2:0] state;
@@ -56,24 +59,22 @@ module cpu3(
 	wire [15:0] de_imm [1:0];
 	wire [4:0] de_opr [1:0];
 	wire [3:0] de_ctrl [1:0];
-	wire [9:0] de_mod [1:0]
+	wire [9:0] de_mod [1:0];
+	wire [72:0] de_data [1:0];
 	//wait
-	reg [5:0] wa_ope [3:0];
-	reg [5:0] wa_ds [3:0];
-	reg [5:0] wa_dt [3:0];
-	reg [5:0] wa_dd [3:0];
-	reg [15:0] wa_imm [3:0];
-	reg [4:0] wa_opr [3:0];
-	reg [3:0] wa_ctrl [3:0];
-	reg [9:0] wa_mod [3:0];
-	wire [63:0] wa_st_board [3:0];
+	reg [72:0] wa_data [3:0];
+	wire [13:0] wa_pc [3:0];
+	wire [5:0] wa_ope [3:0];
+	wire [5:0] wa_ds [3:0];
+	wire [5:0] wa_dt [3:0];
+	wire [5:0] wa_dd [3:0];
+	wire [15:0] wa_imm [3:0];
+	wire [4:0] wa_opr [3:0];
+	wire [3:0] wa_ctrl [3:0];
+	wire [9:0] wa_mod [3:0];
+	wire [63:0] wa_std_board [3:0];
 	wire wa_is_busy;
 
-	//branch prediction
-	wire bp_is_branch;
-	wire bp_is_b_op;
-	wire bp_is_taken;
-	// module bp2
 
 	//board {fpr,gpr}
 	reg [63:0] board;
@@ -86,8 +87,8 @@ module cpu3(
 	assign i_en = 1'b1;
 
 	//if
-	assign if_is_en[0] = if_pc[0] == 0 && ~b_is_hazard && ~bp_is_taken;
-	assign if_is_en[1] = ~b_is_hazard && ~bp_is_taken;
+	assign if_is_en[0] = if_pc[0] == 0 && ~b_is_hazard;
+	assign if_is_en[1] = ~b_is_hazard;
 
 	//de
 	assign de_is_j[0] = de_is_en[0] && de_instr[0][31:30] == 2'b00 && de_instr[0][27:26] == 2'b10;
@@ -130,13 +131,51 @@ module cpu3(
 	assign de_ctrl[1] = de_instr[1][6:3];
 	assign de_mod[0] = //////////////
 	assign de_mod[1] = //////////////
+	assign de_data[0] = {{de_pc[13:1],1'b0},de_ope[0],de_ds[0],de_dt[0],de_dd[0],de_imm[0],de_opr[0],de_ctrl[0],de_mod[0]};
+	assign de_data[1] = {{de_pc[13:1],1'b1},de_ope[1],de_ds[1],de_dt[1],de_dd[1],de_imm[1],de_opr[1],de_ctrl[1],de_mod[1]};
 	
 		
 	//wa
-	assign wa_st_board[0] <= (1 << wa_ds[0]) & (1 << wa_dt[0]);
-	assign wa_st_board[1] <= (1 << wa_ds[1]) & (1 << wa_dt[1]);
-	assign wa_st_board[2] <= (1 << wa_ds[2]) & (1 << wa_dt[2]);
-	assign wa_st_board[3] <= (1 << wa_ds[3]) & (1 << wa_dt[3]);
+	assign wa_pc[0] = wa_data[0][72:59];
+	assign wa_ope[0] = wa_data[0][58:53];
+	assign wa_ds[0] = wa_data[0][52:47];
+	assign wa_dt[0] = wa_data[0][46:41];
+	assign wa_dd[0] = wa_data[0][40:35];
+	assign wa_imm[0] = wa_data[0][34:19];
+	assign wa_opr[0] = wa_data[0][18:14];
+	assign wa_ctrl[0] = wa_data[0][13:10];
+	assign wa_mod[0] = wa_data[0][9:0];
+	assign wa_pc[1] = wa_data[1][72:59];
+	assign wa_ope[1] = wa_data[1][58:53];
+	assign wa_ds[1] = wa_data[1][52:47];
+	assign wa_dt[1] = wa_data[1][46:41];
+	assign wa_dd[1] = wa_data[1][40:35];
+	assign wa_imm[1] = wa_data[1][34:19];
+	assign wa_opr[1] = wa_data[1][18:14];
+	assign wa_ctrl[1] = wa_data[1][13:10];
+	assign wa_mod[1] = wa_data[1][9:0];
+	assign wa_pc[2] = wa_data[2][72:59];
+	assign wa_ope[2] = wa_data[2][58:53];
+	assign wa_ds[2] = wa_data[2][52:47];
+	assign wa_dt[2] = wa_data[2][46:41];
+	assign wa_dd[2] = wa_data[2][40:35];
+	assign wa_imm[2] = wa_data[2][34:19];
+	assign wa_opr[2] = wa_data[2][18:14];
+	assign wa_ctrl[2] = wa_data[2][13:10];
+	assign wa_mod[2] = wa_data[2][9:0];
+	assign wa_pc[3] = wa_data[3][72:59];
+	assign wa_ope[3] = wa_data[3][58:53];
+	assign wa_ds[3] = wa_data[3][52:47];
+	assign wa_dt[3] = wa_data[3][46:41];
+	assign wa_dd[3] = wa_data[3][40:35];
+	assign wa_imm[3] = wa_data[3][34:19];
+	assign wa_opr[3] = wa_data[3][18:14];
+	assign wa_ctrl[3] = wa_data[3][13:10];
+	assign wa_mod[3] = wa_data[3][9:0];
+	assign wa_std_board[0] <= (1 << wa_ds[0]) & (1 << wa_dt[0]) & (1 << wa_dd[0]) & mask;
+	assign wa_std_board[1] <= (1 << wa_ds[1]) & (1 << wa_dt[1]) & (1 << wa_dd[1]) & mask;
+	assign wa_std_board[2] <= (1 << wa_ds[2]) & (1 << wa_dt[2]) & (1 << wa_dd[2]) & mask;
+	assign wa_std_board[3] <= (1 << wa_ds[3]) & (1 << wa_dt[3]) & (1 << wa_dd[3]) & mask;
 	assign wa_is_busy = //////////////////
 
 	integer i1,i2;
@@ -152,14 +191,7 @@ module cpu3(
 				de_is_en[i1] <= 0;
 			end
 			for(i2=0;i2 < 4; i2=i2+1) begin
-				wa_ope[i2] <= 0;
-				wa_ds[i2] <= 0;
-				wa_dt[i2] <= 0;
-				wa_dd[i2] <= 0;
-				wa_imm[i2] <= 0;
-				wa_opr[i2] <= 0;
-				wa_ctrl[i2] <= 0;
-				wa_mod[i2] <= 0;
+				wa_data[i2] <= 0;
 			end
 			board <= 0;
 		end else if(state == st_begin) begin
@@ -173,9 +205,9 @@ module cpu3(
 			de_pc <= if_pc;
 			de_instr[0] <= wa_is_busy ? de_instr[0] : i_rdata[63:32];
 			de_instr[1] <= wa_is_busy ? de_instr[1] : i_rdata[31:0];
-			// enなのはifステージでenかつ制御ハザードが起きず、前の命令の分岐予測がnot taken
-			de_is_en[0] <= wa_is_busy ? de_is_en[0] : if_is_en[0] & ~b_is_hazard & ~bp_is_taken;
-			de_is_en[1] <= wa_is_busy ? de_is_en[1] : if_is_en[1] & ~b_is_hazard & ~bp_is_taken;
+			// enなのはifステージでenかつ制御ハザードが起きない
+			de_is_en[0] <= wa_is_busy ? de_is_en[0] : if_is_en[0] & ~b_is_hazard;
+			de_is_en[1] <= wa_is_busy ? de_is_en[1] : if_is_en[1] & ~b_is_hazard;
 			//wait
 			
 			board <= //////////
